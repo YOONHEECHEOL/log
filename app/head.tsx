410c2def11ce9921b4d771f829c8167601669f92
hcyoon0511 <hcyoon0511@douzone.com>
2023-02-28 10:51:02 +0900

    google Analytics 제거

--- app/head.tsx
+++ app/head.tsx
@@ -1,32 +1,9 @@
-import * as gtag from 'lib/gtag'
-import Script from "next/script"
-
 const Head = () => {
 
     return (
             <>
                 <title>Yoon log</title>
                 <meta charSet="utf-8" />
-
-                {/* google Analytics */}
-                <Script
-                    strategy="afterInteractive"
-                    src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-                />
-                <Script
-                    id="gtag-init"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                    __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', '${gtag.GA_TRACKING_ID}', {
-                        page_path: window.location.pathname,
-                        });
-                    `,
-                    }}
-                />
             </>
         )
 }
