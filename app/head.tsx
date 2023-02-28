0e5ef5298211d3ebac5f8eb319eb5d8e9665a015
hcyoon0511 <hcyoon0511@douzone.com>
2023-02-28 11:08:02 +0900

    vercel Analytics 수정

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+
 const Head = () => {
 
     return (
             <>
                 <title>Yoon log</title>
                 <meta charSet="utf-8" />
+                <Analytics />
             </>
         )
 }
