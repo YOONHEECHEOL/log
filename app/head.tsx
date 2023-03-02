f297f6993d208472d9da10046ed595e04ff60c53
hcyoon0511 <hcyoon0511@douzone.com>
2023-03-02 15:13:35 +0900

    meta tag update

--- app/head.tsx
+++ app/head.tsx
@@ -4,8 +4,23 @@ const Head = () => {
 
     return (
             <>
-                <title>Yoon log</title>
+                <title>yoon-hee-cheol blog</title>
                 <meta charSet="utf-8" />
+
+                <meta name='description' content='' />            
+                <meta name='author' content='YOON HEE CHEOL' />
+                <meta name='keywords' content='FrontEnd Front front Developer 개발 프론트엔드 개발자 React Javascript Next js JS' />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://yoon-hee-cheol.xyz" />
+                <meta property="og:title" content="yoon-hee-cheol" />
+                <meta property="og:image" content="https://example.com/image.jpg" />
+                <meta property="og:site_name" content="yoon-hee-cheol.xyz" />
+                <meta property="og:description" content="FrontEnd FE 개발자 윤희철입니다. javascript, react, next 등을 다룹니다." />
+                <meta property="og:locale" content="en_US" />
+                {/* <!-- 다음의 태그는 필수는 아니지만, 포함하는 것을 추천함 --> */}
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
                 <Analytics />
             </>
         )
