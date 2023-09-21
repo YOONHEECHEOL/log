df155dcdd91422fa4653644e674e8f860759ddb2
YOONHEECHEOL <luk2903201@gmail.com>
2023-09-22 00:59:49 +0900

    temp

--- app/posts/[year]/[month]/[slug]/page.tsx
+++ app/posts/[year]/[month]/[slug]/page.tsx
@@ -1,10 +1,9 @@
-import getPostMetaData from '@/app/utils/getPostMetaData';
-import fs from 'fs';
-import matter from 'gray-matter';
-import Markdown from 'markdown-to-jsx';
-import Tags from '@/app/layout/Tags';
-import { CodeBlock, PreBlock } from '@/components/CodeBlock';
-
+import getPostMetaData from "@/app/utils/getPostMetaData";
+import fs from "fs";
+import matter from "gray-matter";
+import Markdown from "markdown-to-jsx";
+import Tags from "@/app/layout/Tags";
+import { CodeBlock, PreBlock } from "@/components/CodeBlock";
 
 export const generateStaticParams = async () => {
     const posts = getPostMetaData();
@@ -12,57 +11,67 @@ export const generateStaticParams = async () => {
     return posts.map((post) => ({
         year: post.year,
         month: post.month,
-        slug: post.slug
-    }))
-}
+        slug: post.slug,
+    }));
+};
 
 const getPostContent = (year: any, month: string, slug: any) => {
-
     const file = `posts/${year}/${month}/${slug}`;
-    const content = fs.readFileSync(file, 'utf-8');
-
+    const content = fs.readFileSync(file, "utf-8");
 
     const matterResult = matter(content);
 
     return matterResult;
-}
+};
 
 interface ChildrenNode {
     className: string;
     children: string | string[];
 }
 const H1 = ({ children, ...props }: ChildrenNode) => {
-    return <pre {...props}>{children}</pre>
+    return <pre {...props}>{children}</pre>;
 };
 
 const Page = (props: any) => {
-
     const { year, month, slug } = props.params;
 
     const post = getPostContent(year, month, slug);
 
-    return <div className='prose min-h-screen max-w-full mx-4'>
-        <h1 className='flex justify-center py-3 font-bold'>{post.data.title}</h1>
-        <Tags />
-        <article className="">
-            <Markdown
-                options={{
-                    overrides: {
-                        h1: {
-                            component: H1,
-                            props: {
-                                className: "foo",
-                            }
+    return (
+        <div className="prose min-h-screen max-w-full mx-4">
+            <h1 className="flex justify-center py-3 font-bold">
+                {post.data.title}
+            </h1>
+            <Tags />
+            <article className="">
+                <Markdown
+                    options={{
+                        overrides: {
+                            h1: {
+                                component: H1,
+                                props: {
+                                    className: "foo",
+                                },
+                            },
+                            // pre: {
+                            //     component: PreBlock,
+                            // },
+                            code: {
+                                component: CodeBlock,
+                                props: {
+                                    className: (props: any) => {
+                                        console.log(">>>" + props);
+                                        return "123";
+                                    },
+                                },
+                            },
                         },
-                        pre: {
-                            component: PreBlock,
-                        },
-                    }
-                }}
-            >
-                {post.content}
-            </Markdown>
-        </article>
-    </div>
-}
+                    }}
+                >
+                    {post.content}
+                </Markdown>
+            </article>
+        </div>
+    );
+};
 export default Page;
--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,28 +1,44 @@
-'use client'
+"use client";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface ChildrenNode {
     className: string;
     children: string | string[];
 }
 export const CodeBlock = ({ className, children }: ChildrenNode) => {
-    let lang = 'text';
+    let lang = "text";
 
-    if (className && className.startsWith('lang-')) {
-        lang = className.replace('lang-', '');
+    // console.log(className);
+    if (className && className.startsWith("lang-")) {
+        lang = className.replace("lang-", "");
     }
+    // console.log(lang);
 
     return (
-        <SyntaxHighlighter language={lang} style={materialDark}>
-            {children}
-        </SyntaxHighlighter>
+        <>
+            <SyntaxHighlighter
+                language={lang.toLowerCase()}
+                style={materialDark}
+            >
+                {children}
+            </SyntaxHighlighter>
+        </>
     );
-}
+};
 
 export const PreBlock = ({ children, ...rest }: any) => {
-    if ('type' in children && children['type'] === 'code') {
-        return CodeBlock(children['props']);
+    if ("type" in children && children["type"] === "code") {
+        // console.log(children["props"]?.className);
+
+        return (
+            <CodeBlock
+                className={"language-javascript"}
+                children={children["props"]?.children}
+            />
+        );
     }
     return <pre {...rest}>{children}</pre>;
+
+    // return CodeBlock(children["props"]);
 };
